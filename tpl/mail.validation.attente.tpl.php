6bb83a7f4e7f0895e526f2d022d8ef2b68d48611
Alexis Algoud <alexis@atm-consulting.fr>
2016-03-01 17:24:04 +0100

    init

---
+++ tpl/mail.validation.attente.tpl.php
@@ -0,0 +1,5 @@
+<hr>
+
+<p>[translate.Hello;strconv=no] [validation.prenom;strconv=no;protect=no] [validation.nom;strconv=no;protect=no].</p>
+
+<p>[translate.NbAbsencesRequestWaitingValidation;strconv=no]</p>
