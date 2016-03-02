cee54d989c5ea8b312ea193eacd6768e09440796
Alexis Algoud <alexis@atm-consulting.fr>
2016-03-02 11:44:12 +0100

    FIX min

--- tpl/calendrier.tpl.php
+++ tpl/calendrier.tpl.php
@@ -7,19 +7,19 @@
 				<tr>
 					<td>[absence.groupe;strconv=no;protect=no]</td>
 					<td>[absence.TGroupe;strconv=no;protect=no]</td>
-				<tr/>
+				</tr>
 				<tr>
 					<td>[absence.utilisateur;strconv=no;protect=no]</td>
 					<td>[absence.TUser;strconv=no;protect=no]</td>
-				<tr/>
+				</tr>
 				<tr>
 					<td>[absence.type;strconv=no;protect=no]</td>
 					<td>[absence.TTypeAbsence;strconv=no;protect=no]</td>
-				<tr/>
+				</tr>
 				<tr>
 					<td></td>
 				 	<td colspan="2">[absence.btValider;strconv=no;protect=no] </td>
-				<tr/>
+				</tr>
 			</table>
 		 	<br> 
 		[onshow;block=end]
