0604e9abc288c3eeca740aa34cebe2e4dc1826ff
root <root@SRVDOLIBARR.groupecpro.local>
2016-05-12 16:27:42 +0200

    FIX inclusion

--- default.config.php
+++ default.config.php
@@ -1,17 +1,19 @@
 <?php
 
 	if(is_file('../main.inc.php'))$dir = '../';
+	else if(is_file( '../../main.inc.php')) $dir = '../../';
 	else  if(is_file('../../../main.inc.php'))$dir = '../../../';
 	else  if(is_file('../../../../main.inc.php'))$dir = '../../../../';
 	else  if(is_file('../../../../../main.inc.php'))$dir = '../../../../../';
-	else $dir = '../../';
-
-
+	else {
+		exit('Impossible to find main.inc');
+	}
 	if(!defined('INC_FROM_DOLIBARR') && defined('INC_FROM_CRON_SCRIPT')) {
 		include($dir."master.inc.php");
 	}
 	elseif(!defined('INC_FROM_DOLIBARR')) {
 		include($dir."main.inc.php");
+
 	} else {
 		global $dolibarr_main_db_host, $dolibarr_main_db_name, $dolibarr_main_db_user, $dolibarr_main_db_pass;
 	}
