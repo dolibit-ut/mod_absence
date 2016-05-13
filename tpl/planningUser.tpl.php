3d8727ff91daa7ddb1bab6e8606f2a05732930be
Alexis Algoud <alexis@atm-consulting.fr>
2016-05-13 16:21:45 +0200

    FIX unselect

--- tpl/planningUser.tpl.php
+++ tpl/planningUser.tpl.php
@@ -13,10 +13,10 @@
 				<td> [translate.EndDate;strconv=no]</td> <td> [recherche.date_fin;block=tr;strconv=no;protect=no]</td>
 			</tr>
 			<tr >
-				<td> [translate.Group;strconv=no]</td> <td> [recherche.TGroupe;block=tr;strconv=no;protect=no]</td>
+				<td> [translate.Group;strconv=no]</td> <td rel="combo-group"> [recherche.TGroupe;block=tr;strconv=no;protect=no]</td>
 			</tr>
 			<tr >
-				<td>[translate.Or;strconv=no] [translate.User;strconv=no]</td> <td> [recherche.TUser;block=tr;strconv=no;protect=no]</td>
+				<td>[translate.Or;strconv=no] [translate.User;strconv=no]</td> <td rel="combo-user"> [recherche.TUser;block=tr;strconv=no;protect=no]</td>
 			</tr>
 			<tr >
 				<td colspan="2" style="text-align:center"> [recherche.btValider;block=tr;strconv=no;protect=no]</td>
@@ -25,3 +25,20 @@
 	</div>
 
 	<br><br><br><br>
+<script type="text/javascript">
+	
+	$(document).ready(function() {
+		
+		$('td[rel=combo-group] select').change(function() {
+			$('td[rel=combo-user] select').val(0);
+		});
+		
+		$('td[rel=combo-user] select').change(function() {
+			$('td[rel=combo-group] select').val(0);
+		});
+		
+		
+	});
+	
+	
+</script>
