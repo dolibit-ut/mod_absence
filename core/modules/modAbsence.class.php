1a7567ccca9e975cb6aaab363e205b0069302beb
Alexis Algoud <alexis@atm-consulting.fr>
2016-06-14 17:10:19 +0200

    NEW menu check

--- core/modules/modAbsence.class.php
+++ core/modules/modAbsence.class.php
@@ -661,6 +661,21 @@ class modAbsence extends DolibarrModules
 					'user'=> 2
         );
 		$r++;
+		$this->menu[$r]=array(
+		            'fk_menu'=>'fk_mainmenu=absence,fk_leftmenu=admin',			// Put 0 if this is a top menu
+		        	'type'=> 'left',			// This is a Top menu entry
+		        	'titre'=> $langs->trans('CheckCompteur'),
+		        	'mainmenu'=> '',
+		        	'leftmenu'=> '',		// Use 1 if you also want to add left menu entries using this descriptor. Use 0 if left menu entries are defined in a file pre.inc.php (old school).
+					'url'=> '/absence/script/check-compteur.php',
+					'langs'=> 'absence@absence',	// Lang file to use (without .lang) by module. File must be in langs/code_CODE/ directory.
+					'position'=> 105,
+					'enabled'=> '1',			// Define condition to show or hide menu entry. Use '$conf->mymodule->enabled' if entry must be visible if module is enabled.
+					'perms'=> '$user->rights->absence->myactions->modifierParamGlobalConges',			// Use 'perms'=>'$user->rights->mymodule->level1->level2' if you want your menu with a permission rules
+					'target'=> '',
+					'user'=> 2
+        );
+		$r++;
 		/*$this->menu[$r]=array(
 		            'fk_menu'=>'fk_mainmenu=absence,fk_leftmenu=admin',			// Put 0 if this is a top menu
 		        	'type'=> 'left',			// This is a Top menu entry
