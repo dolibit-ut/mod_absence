b6e6ff93a82f539c48a3e2ca76beb212d16cb211
Alexis Algoud <alexis@atm-consulting.fr>
2016-10-04 16:01:26 +0200

    remplacement wdCalendar FullCalendar

--- script/absenceCalendarDataFeed.php
+++ script/absenceCalendarDataFeed.php
@@ -5,80 +5,24 @@ dol_include_once("/absence/class/absence.class.php");
 dol_include_once("/valideur/class/valideur.class.php");
 dol_include_once("/rhlibrary/wdCalendar/php/functions.php");
 
-$ATMdb=new TPDOdb;
+$PDOdb=new TPDOdb;
 
-$method = $_GET["method"];
+$method = GETPOST('action');
 switch ($method) {
-    case "list":
-	       	$ret = listCalendar($ATMdb, $_REQUEST["showdate"], $_REQUEST["viewtype"], $_REQUEST['idUser'], $_REQUEST['idGroupe'], $_REQUEST['typeAbsence']);
+    case "afficher":
+	       	__out(listCalendarByRange($PDOdb, GETPOST('start'), GETPOST('end'), GETPOST('idUtilisateur'), GETPOST('groupe'), GETPOST('typeAbsence')),'json');
 
         break; 
-		
-	case 'add':
-			
-			$ret = addCalendar($ATMdb, $_REQUEST['CalendarTitle'], $_REQUEST['CalendarStartTime'], $_REQUEST['CalendarEndTime'], $_REQUEST['isAllDayEvent']);
-			
-		break;
 
 }
-echo json_encode($ret); 
 
-function addCalendar(&$ATMdb, $title, $date_start,$date_end, $fulldayevent=1, $actionCode=50) {
-global $db, $user;	
-	
-	require_once DOL_DOCUMENT_ROOT.'/comm/action/class/cactioncomm.class.php';
-	require_once DOL_DOCUMENT_ROOT.'/comm/action/class/actioncomm.class.php';
-	
-	
-	$t_start = strtotime($date_start);
-	$t_end = strtotime($date_end);
-	
-	$cactioncomm = new CActionComm($db);
-	$actioncomm = new ActionComm($db);
-	
-	$result=$cactioncomm->fetch($actionCode);
-	
-	$actioncomm->label = $title;
-	
-	$actioncomm->type_id = $cactioncomm->id;
-	$actioncomm->type_code = $cactioncomm->code;
-	$actioncomm->priority = 0;
-	$actioncomm->fulldayevent = $fulldayevent;
-	$actioncomm->location = '';
-	$actioncomm->transparency = 1;
-	
-	$actioncomm->datep = $t_start;
-	$actioncomm->datef = $t_end;
-	$actioncomm->percentage = 0;
-	$actioncomm->duree=$t_end - $t_start;
-
-	$actioncomm->usertodo = $user;
-
-	$idaction=$actioncomm->add($user);
-	
-	$ret = array();
-	  $ret['IsSuccess'] = true;
-	  $ret['Msg'] = 'add success';
-	  $ret['Data'] = 'url:'.dol_buildpath('/comm/action/fiche.php?action=edit&id='.$idaction,1) ;
-	  	
-	
-	return $ret;
-}
-
-
-function listCalendarByRange(&$ATMdb, $date_start, $date_end, $idUser=0, $idGroupe=0, $typeAbsence = 'Tous'){
-  global $conf,$user, $langs;
+function listCalendarByRange(&$PDOdb, $date_start, $date_end, $idUser=0, $idGroupe=0, $typeAbsence = 'Tous'){
+  global $conf,$user, $langs,$db;
     
-  $ret = array();
-  $ret['events'] = array();
-  $ret["issort"] =true;
-  $ret["start"] = _justDate($date_start);
-  $ret["end"] =  _justDate($date_end);
-  $ret['error'] = null;
+ 	$TEvent=array();
   
-  
-  	$TJourFerie=getJourFerie($ATMdb, $date_start, $date_end);
-	$ret['events'] = array_merge($TJourFerie, $ret['events']); 
+  	$TJourFerie=getJourFerie($PDOdb, $date_start, $date_end);
+	$TEvent = array_merge($TJourFerie, $TEvent); 
 
   	if($user->rights->absence->myactions->voirToutesAbsences){		//si on a le droit de voir toutes les absences
 	  	
@@ -110,7 +54,7 @@ function listCalendarByRange(&$ATMdb, $date_start, $date_end, $idUser=0, $idGrou
 	}
 	else if($user->rights->absence->myactions->voirGroupesAbsences) {
 			
-		$Tab = $ATMdb->ExecuteAsArray("SELECT fk_usergroup FROM ".MAIN_DB_PREFIX."usergroup_user WHERE fk_user=".$user->id);
+		$Tab = $PDOdb->ExecuteAsArray("SELECT fk_usergroup FROM ".MAIN_DB_PREFIX."usergroup_user WHERE fk_user=".$user->id);
 		$TGroup=array(0);
 		foreach($Tab as $row)$TGroup[] = $row->fk_usergroup;		
 				
@@ -156,7 +100,7 @@ function listCalendarByRange(&$ATMdb, $date_start, $date_end, $idUser=0, $idGrou
 	      
 	}
   	
-  	$TRow = $ATMdb->ExecuteAsArray($sql1);
+  	$TRow = $PDOdb->ExecuteAsArray($sql1);
     
 	
 
@@ -167,6 +111,10 @@ function listCalendarByRange(&$ATMdb, $date_start, $date_end, $idUser=0, $idGrou
 		if($row->etat=='Refusee' && !$user->rights->absence->myactions->voirAbsenceRefusee){
 			continue;
 		}
+		
+		$userAbs=new User($db);
+		$userAbs->fetch($row->fk_user);
+				
 		
 		if($row->isPresence==1) {
 	
@@ -185,9 +133,9 @@ function listCalendarByRange(&$ATMdb, $date_start, $date_end, $idUser=0, $idGrou
 				$timeFin = strtotime(date('Y-m-d',$t_current).' '.substr($row->date_hourEnd,11) ) ; 
 				
 				$url = "presence.php?id=".$row->rowid."&action=view";//$row->location,
-		        	$attends = 'presence';//$attends
+		        $attends = 'presence';//$attends
 
-				if($user->id!=$row->fk_user && !TRH_valideur_groupe::isValideur($ATMdb, $user->id)) {
+				if($user->id!=$row->fk_user && !TRH_valideur_groupe::isValideur($PDOdb, $user->id)) {
 					$label = $row->lastname.' '.$row->firstname;
                                 }
                                 else {
@@ -198,26 +146,36 @@ function listCalendarByRange(&$ATMdb, $date_start, $date_end, $idUser=0, $idGrou
 	
 
 				if($moreOneDay) {
-					$label.=' du '._justDate($timeDebut,'d/m').' au '._justDate($timeFin,'d/m/Y');
+					$label.=' du '.dol_print_date($timeDebut).' au '.dol_print_date($timeFin);
 				}
 				
 				if(mb_detect_encoding($label,'UTF-8', true) === false  ) $label = utf8_encode($label);
 				
-				$ret['events'][] = array(
-			        $row->rowid,
-			        $label,
-			        _justDate($timeDebut),
-			        _justDate($timeFin),
-			        0,//$row->isAllDayEvent,
-			        0, //more than one day event
-			        //$row->InstanceType,
-			        $row->fk_user,//Recurring event,
-			        $color,//$row->color,
-			        0//editable
-			        ,$url
-			        ,$attends
-			        ,$row->fk_user
-			      );
+				
+				
+				$TEvent[]=array(
+					'id'=>$row->rowid
+					,'title'=>$label
+					,'allDay'=>0
+					,'start'=>(empty($timeDebut) ? '' : date('Y-m-d H:i:s',(int)$timeDebut))
+					,'end'=>(empty($timeFin) ? '' : date('Y-m-d H:i:s',(int)$timeFin))
+					,'url'=>$url
+					,'editable'=>0
+					,'color'=>'#66ff66'
+					,'isDarkColor'=>0
+					,'colors'=>''
+					,'note'=>''
+					,'statut'=>''
+					,'fk_soc'=>0
+					,'fk_contact'=>0
+					,'fk_user'=>$row->fk_user
+					,'fk_project'=>0
+					,'societe'=>''
+					,'contact'=>''
+					,'user'=>$userAbs->getFullName($langs)
+					,'project'=>''
+					,'more'=>''
+				);
 				
 				$t_current=strtotime('+1day', $t_current);
 			}
@@ -228,13 +186,13 @@ function listCalendarByRange(&$ATMdb, $date_start, $date_end, $idUser=0, $idGrou
 			
 			switch($row->etat){
 				case 'Avalider' : 
-					$color=6;
+					$color = '#ff44ff';
 					break;
 				case 'Refusee':
-					$color=14;
+					$color = '#ff4444';
 					break;
 				default:
-					$color=8;
+					$color='';
 					break;
 			}
 			
@@ -251,52 +209,60 @@ function listCalendarByRange(&$ATMdb, $date_start, $date_end, $idUser=0, $idGrou
 			$url = "absence.php?id=".$row->rowid."&action=view";//$row->location,
 		        $attends = 'absence';//$attends
 				
-			if($user->id!=$row->fk_user && !TRH_valideur_groupe::isValideur($ATMdb, $user->id)) {
+			if($user->id!=$row->fk_user && !TRH_valideur_groupe::isValideur($PDOdb, $user->id)) {
                      $label = $row->lastname.' '.$row->firstname;
 				     $url = '#';
             }
             else {
-                     $label = $row->lastname.' '.$row->firstname.' : '.$row->libelle;
+                     $label = $row->lastname.' '.$row->firstname.' : '.html_entity_decode( $row->libelle);
 
             }
 			
 			if(mb_detect_encoding($label,'UTF-8', true) === false  ) $label = utf8_encode($label);
 			
-//	var_dump($label, $user->id,$row->fk_user,TRH_valideur_groupe::isValideur($ATMdb, $row->fk_user), '<br>');        
+//	var_dump($label, $user->id,$row->fk_user,TRH_valideur_groupe::isValideur($PDOdb, $row->fk_user), '<br>');        
 	//	        $label = utf8_encode($row->lastname.' '.$row->firstname).' : '.$row->libelle;
 			if($moreOneDay) {
 				$label.=' du '._justDate($timeDebut,'d/m').' au '._justDate($timeFin,'d/m/Y');
 			}
 			
-			$ret['events'][] = array(
-		        $row->rowid,
-		        $label,
-		        _justDate($timeDebut),
-		        _justDate($timeFin),
-		        $allDayEvent,//$row->isAllDayEvent,
-		        $moreOneDay, //more than one day event
-		        //$row->InstanceType,
-		        $row->fk_user,//Recurring event,
-		        $color,//$row->color,
-		        0//editable
-		        ,$url
-		        ,$attends
-		        ,$row->fk_user
-		      );
+			$TEvent[]=array(
+				'id'=>$row->rowid
+				,'title'=>$label
+				,'allDay'=>0
+				,'start'=>(empty($timeDebut) ? '' : date('Y-m-d H:i:s',(int)$timeDebut))
+				,'end'=>(empty($timeFin) ? '' : date('Y-m-d H:i:s',(int)$timeFin))
+				,'url'=>$url
+				,'editable'=>0
+				,'color'=>$color
+				,'isDarkColor'=>0
+				,'colors'=>''
+				,'note'=>''
+				,'statut'=>''
+				,'fk_soc'=>0
+				,'fk_contact'=>0
+				,'fk_user'=>$row->fk_user
+				,'fk_project'=>0
+				,'societe'=>''
+				,'contact'=>''
+				,'user'=>$userAbs->getFullName($langs)
+				,'project'=>''
+				,'more'=>''
+			);
 	        
 		}	
 		
 	  }  
-	    
+	 
 	if($conf->agenda->enabled && $_REQUEST['withAgenda']==1) {
-	    $TAgenda=getAgendaEvent($ATMdb, $date_start, $date_end);
-		$ret['events'] = array_merge($ret['events'], $TAgenda); 
+	    $TAgenda=getAgendaEvent($PDOdb, $date_start, $date_end);
+		$TEvent = array_merge($TEvent, $TAgenda); 
 		//$ret['events'] = $TAgenda;
 		
 	}  
 	
 
-  return $ret;
+  return $TEvent;
 }
 
 function _justDate($date,$frm = 'm/d/Y H:i') {
@@ -306,79 +272,76 @@ function _justDate($date,$frm = 'm/d/Y H:i') {
 	return date($frm, $time);
 }
 
-function getJourFerie(&$ATMdb, $date_start, $date_end) {
+function getJourFerie(&$PDOdb, $date_start, $date_end) {
 	global $conf, $langs;	
 	
-	$Tab=array();
+	$TEvent=array();
 
-	$TJF=TRH_JoursFeries::getAll($ATMdb, $date_start, $date_end);
+	$TJF=TRH_JoursFeries::getAll($PDOdb, $date_start, $date_end);
 		  //récupération des jours fériés 
 	foreach($TJF as $row) {
+		
+		  $timeOff = strtotime($row->date_jourOff);	
+			
+		  $allDay = 1;
 		  switch($row->moment){
 			case 'apresmidi' : 
-				$moment= $langs->trans('ClosedTheAfternoon');
+				$moment= $langs->transnoentities('ClosedTheAfternoon');
+				
+				$allDay = 0;
+				$start = date('Y-m-d 12:00:00', $timeOff);
+				$end = date('Y-m-d 23:59:59', $timeOff);
+				
 				break;
 			case 'matin':
-				$moment= $langs->trans('ClosedTheMorning');
+				$moment= $langs->transnoentities('ClosedTheMorning');
+				
+				$allDay = 0;
+				$start = date('Y-m-d 00:00:00', $timeOff);
+				$end = date('Y-m-d 12:00:00', $timeOff);
+				
 				break;
-			case 'allday':
-				$moment= $langs->trans('PublicHoliday');
+			default:
+				$moment= $langs->transnoentities('PublicHoliday');
+				
+				$allDay = 1;
+				$start = date('Y-m-d 00:00:00', $timeOff);
+				$end = date('Y-m-d 23:59:59', $timeOff);
+				
 				break;
     		}
 		  
-		  
-	      $Tab[] = array(
-	        100000+$row->rowid,
-	        $moment.' : '.$row->commentaire,
-	        _justDate($row->date_jourOff),
-	       	_justDate($row->date_jourOff),
-	        1,//$row->isAllDayEvent,
-	        0, //more than one day event
-	        //$row->InstanceType,
-	        0,//Recurring event,
-	        1,//$row->color,
-	        0,//editable
-	        "joursferies.php?idJour=".$row->rowid."&action=view",//$row->location,
-	        '',//$attends
-	      );
-	  
+		  	$TEvent[]=array(
+				'id'=>100000000+$row->rowid
+				,'title'=>$moment
+				,'allDay'=>$allDay
+				,'start'=>$start
+				,'end'=>$end
+				,'url'=>'#'
+				,'editable'=>0
+				,'color'=>''
+				,'isDarkColor'=>1
+				,'colors'=>'#333333'
+				,'note'=>$row->commentaire
+				,'statut'=>''
+				,'fk_soc'=>0
+				,'fk_contact'=>0
+				,'fk_user'=>0
+				,'fk_project'=>0
+				,'societe'=>''
+				,'contact'=>''
+				,'user'=>''
+				,'project'=>''
+				,'more'=>''
+			);
+	      
      }
-	
-	return $Tab;
+	//var_dump($TEvent);
+	return $TEvent;
 	
 }
 
-function listCalendar(&$ATMdb, $day, $type, $idAbsence, $idGroupe, $typeAbsence){
-  	
-  $phpTime = js2PhpTime($day);
-  switch($type){
-    case "week":
-      //suppose first day of a week is monday 
-      $monday  =  date("d", $phpTime) - date('N', $phpTime) + 1;
-      //echo date('N', $phpTime);
-      $st = mktime(0,0,0,date("m", $phpTime), $monday, date("Y", $phpTime));
-      $et = mktime(0,0,-1,date("m", $phpTime), $monday+7, date("Y", $phpTime));
-      break;
-    case "day":
-      $st = mktime(0, 0, 0, date("m", $phpTime), date("d", $phpTime), date("Y", $phpTime));
-      $et = mktime(0, 0, -1, date("m", $phpTime), date("d", $phpTime)+1, date("Y", $phpTime));
-      break;
-	default:  
-	
-		$st = strtotime( date('Y-m-01 00:00:00', strtotime($day)));
-		$et =  strtotime( date('Y-m-t 23:59:59', strtotime($day)));
-	  
-  }
-
-  $date_start = date('Y-m-d 00:00:00', $st);
-  $date_end = date('Y-m-d 23:59:59', $et);
-	  
-	//  $ATMdb->debug=true;
-
-	return listCalendarByRange($ATMdb, $date_start, $date_end, $idAbsence, $idGroupe, $typeAbsence);
-}
-
-function getAgendaEvent(&$ATMdb, $date_start, $date_end) {
+function getAgendaEvent(&$PDOdb, $date_start, $date_end) {
 global $user, $conf;
 		
 	
@@ -427,8 +390,8 @@ global $user, $conf;
 	$sql.= ' WHERE a.fk_action = ca.id';
 	$sql.= ' AND a.fk_user_author = u.rowid';
 	$sql.= ' AND a.entity IN ('.getEntity().')';
-	if ($actioncode) $sql.=" AND ca.code=".$ATMdb->quote($actioncode);
-	if ($pid) $sql.=" AND a.fk_project=".$ATMdb->quote($pid);
+	if ($actioncode) $sql.=" AND ca.code=".$PDOdb->quote($actioncode);
+	if ($pid) $sql.=" AND a.fk_project=".$PDOdb->quote($pid);
 	if (! $user->rights->societe->client->voir && ! $socid) $sql.= " AND (a.fk_soc IS NULL OR sc.fk_user = " .$user->id . ")";
 	if ($user->societe_id) $sql.= ' AND a.fk_soc = '.$user->societe_id; // To limit to external user company
 	
@@ -459,8 +422,8 @@ global $user, $conf;
 	$sql.= ' ORDER BY datep';
 	
 	
-	$ATMdb->Execute($sql);
-	$Tab = $ATMdb->Get_All();
+	$PDOdb->Execute($sql);
+	$Tab = $PDOdb->Get_All();
 	
 	$TEvent=array();
 	
@@ -471,22 +434,32 @@ global $user, $conf;
 		
 		 if($row->code=='AC_OTH_AUTO')$color=5;
 		 else $color = -1; 
+			
+			
+		  $TEvent[]=array(
+				'id'=>200000000 + $row->rowid
+				,'title'=>utf8_encode( $row->label )
+				,'allDay'=>$row->fulldayevent
+				,'start'=>$row->datep
+				,'end'=> $row->datep2
+				,'url'=>dol_buildpath('/comm/action/fiche.php?id='.$row->id,1)
+				,'editable'=>0
+				,'color'=>$color
+				,'isDarkColor'=>0
+				,'colors'=>''
+				,'note'=>''
+				,'statut'=>''
+				,'fk_soc'=>0
+				,'fk_contact'=>0
+				,'fk_user'=>$row->fk_user
+				,'fk_project'=>0
+				,'societe'=>''
+				,'contact'=>''
+				,'user'=>$userAbs->getFullName($langs)
+				,'project'=>''
+				,'more'=>''
+			);
 		
-		 $TEvent[] = array(
-	        200000+$row->id
-	        ,utf8_encode( $row->label )
-	        ,_justDate( $row->datep)
-	        ,_justDate( $row->datep2)
-	        ,$row->fulldayevent
-	        ,0, //more than one day event
-	        //$row->InstanceType,
-	        0,//Recurring event,
-	        $color,//$row->color,
-	        1,//editable
-	        ($canedit ? dol_buildpath('/comm/action/fiche.php?action=edit&id='.$row->id,1) : dol_buildpath('/comm/action/fiche.php?id='.$row->id,1) )  //dol_build_path('/comm/action/fiche.php?id='.$row->id)
-	        ,
-	        '',//$attends
-	     );
 	  	
 		
 	}
