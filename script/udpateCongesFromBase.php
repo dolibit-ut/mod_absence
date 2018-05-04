8a79018871add149dc1b25ac7cea64585ddbd697
root <root@SRVDOLIBARR.groupecpro.local>
2018-05-04 15:36:00 +0200

    FIX : tk7756 : adaptation du script de correction des compteurs

--- script/udpateCongesFromBase.php
+++ script/udpateCongesFromBase.php
@@ -12,10 +12,11 @@ $ATMdb2=new TPDOdb;
 	
 	while($obj = $ATMdb->Get_line()) {
 		
-		$nb_conges_n = _get_conges($ATMdb2, $obj->fk_user,"'conges','cppartiel'",'2017-06-01',null,'congesPrisN','date_fin');
-		$nb_conges_nm1 = _get_conges($ATMdb2, $obj->fk_user,"'conges','cppartiel'",'2016-06-01',null,'congesPrisN');
-		$nb_rtt = _get_conges($ATMdb2, $obj->fk_user,"'rttcumule'",'2016-01-01');
-		$nb_rttnon = _get_conges($ATMdb2, $obj->fk_user,"'rttnoncumule'",'2016-01-01');
+		$nb_conges_n = _get_conges($ATMdb2, $obj->fk_user,"'conges','cppartiel'",'2018-06-01',null,'congesPrisN','date_fin');
+		$nb_conges_nm1 = _get_conges($ATMdb2, $obj->fk_user,"'conges','cppartiel'",'2017-06-01','2018-05-31', 'congesPrisNM1','date_debut');
+		$nb_conges_nm1 += _get_conges($ATMdb2, $obj->fk_user,"'conges','cppartiel'",'2017-06-01','2018-05-31', 'congesPrisN','date_debut',array('2016-06-01','2017-05-31'));
+		$nb_rtt = _get_conges($ATMdb2, $obj->fk_user,"'rttcumule'",'2018-01-01');
+		$nb_rttnon = _get_conges($ATMdb2, $obj->fk_user,"'rttnoncumule'",'2018-01-01');
 		
 		if($nb_conges_n + $nb_conges_nm1 + $nb_rtt+ $nb_rttnon == 0) continue;
 
@@ -35,13 +36,21 @@ $ATMdb2=new TPDOdb;
 		print $sql.'<br />';
 		
 	}
-function _get_conges(&$ATMdb, $fk_user, $type="'conges','cppartiel'", $date= '2015-06-01', $date_max=null, $field = 'nb', $date_field = 'date_debut') {
-	$sql = "SELECT SUM(duree) as nb, SUM(congesPrisN) as congesPrisN FROM llx_rh_absence WHERE fk_user=$fk_user AND type IN ($type)";
+function _get_conges(&$ATMdb, $fk_user, $type="'conges','cppartiel'", $date= '2015-06-01', $date_max=null, $field = 'duree', $date_field = 'date_debut', $date_limit=false) {
+	$sql = "SELECT SUM(".$field.") as nb FROM llx_rh_absence WHERE fk_user=$fk_user AND type IN ($type)";
 	$sql.=" AND ".$date_field.">='$date'";
 	if(!empty($date_max))
 		$sql.=" AND date_debut<='$date_max'";
+
+	if(is_array($date_limit)) {
+		$sql.=" AND date_cre BETWEEN '".$date_limit[0]."' AND '".$date_limit[1]."'";
+	}
+
 	$sql.=" AND etat!='Refusee' ";
 	$ATMdb->Execute($sql);
 	$ATMdb->Get_line();
-	return (float)$ATMdb->Get_field($field);
+
+	
+
+	return (float)$ATMdb->Get_field('nb');
 }
