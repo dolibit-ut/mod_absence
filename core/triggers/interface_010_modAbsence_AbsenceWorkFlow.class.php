96be587b3d1d6b1f7e5832cdd1b824b4f7293d63
atm-greg <gregory.blemand@atm-consulting.fr>
2020-09-11 10:58:06 +0200

    fix calcl heure présence

--- core/triggers/interface_010_modAbsence_AbsenceWorkFlow.class.php
+++ core/triggers/interface_010_modAbsence_AbsenceWorkFlow.class.php
@@ -2,7 +2,7 @@
 
 //define('INC_FROM_DOLIBARR', true);
 //define('TRIGGER', true);
-		
+
 //require('../valideur/config.php');
 //require('../valideur/lib/valideur.lib.php');
 
@@ -78,87 +78,88 @@ class InterfaceAbsenceWorkflow
     function run_trigger($action, &$object, $user, $langs, $conf)
     {
         global $db,$conf,$langs;
-		
+
 		if ($action === 'USER_CREATE' || $action === 'USER_MODIFY') {
 			dol_syslog("Trigger '".$this->name."' for action '$action' launched by ".__FILE__.". id=".$object->rowid);
-			
-			
+
+
 			if($action === 'USER_CREATE' && !empty($conf->global->ABSENCE_USER_CREATE_TR_ON)) {
-				
+
 				$object->fetch_optionals();
 				$object->array_options['options_ticketresto_ok'] = 1;
 				$object->insertExtraFields();
-				
+
 			}
-			
-			
+
+
 			define('INC_FROM_DOLIBARR', true);
 	        dol_include_once('/absence/config.php');
 		    dol_include_once('/valideur/lib/valideur.lib.php');
 			dol_include_once('/absence/class/absence.class.php');
-			
-				
+
+
 			$PDOdb=new TPDOdb;
-			
+
 			if($object->id>0) {
 				$compteur=new TRH_Compteur;
 				if(!$compteur->load_by_fkuser($PDOdb, $object->id)) {
-					
+
 						$compteur->initCompteur($PDOdb,$object->id );
-					
+
 						$compteur->save($PDOdb);
-					
+
 				}
-				
+
 				$emploi = new TRH_EmploiTemps;
-				
+
 				if(!$emploi->load_by_fkuser($PDOdb, $object->id)) {
-					
+
 						$emploi->initCompteurHoraire($PDOdb,$object->id );
-					
+
 						$emploi->save($PDOdb);
-					
+
 				}
-				
-				
+
+
 			}
-			
-		} 
+
+		}
 		elseif ($action === 'ABSENCE_BEFORECREATE') {
-				
+
 			global $user, $db,$langs;
-				
+
 			define('INC_FROM_DOLIBARR', true);
-	        dol_include_once('/absence/config.php');	
-		
+	        dol_include_once('/absence/config.php');
+
 			$PDOdb=new TPDOdb;
+			/** @var TRH_Absence $object */
 			$demandeRecevable=$object->testDemande($PDOdb, $object->fk_user, $object);
 
 			if($demandeRecevable==1 || $demandeRecevable==2){
-						
+
 				if($demandeRecevable==2) {
 					$object->avertissementInfo.=$langs->trans('AbsencesPresencesRequestRulesMaxTimeOut');
 					$object->avertissement=1;
-				}	
-				
+				}
+
 				return 1;
 			}
 			else{
-				
+
 				$this->error =$langs->trans('AbsencesPresencesRequestRulesMaxTimeOutRestrictif');;
-				
+
 				return -1;
 			}
-			
-			
+
+
 		}
 		elseif ($action === 'ABSENCE_BEFOREVALIDATE') {
-			
+
 			if($object->isPresence==0) {
 				$u=new User($db);
 				$u->fetch($object->fk_user);
 				$u->getrights('absence');
-				
+
 				/*
 				 * Supression de ce droit trop dangeureux ** AlertAllMyCoWorker
 				if($u->rights->absence->myactions->alertAllMyCoWorker) {
@@ -166,7 +167,7 @@ class InterfaceAbsenceWorkflow
 					dol_include_once('/absence/config.php');
 
                 	$PDOdb=new TPDOdb;
-                                	
+
 					// Gestion type absence
 					$the_type='absent';
         	        $typeAbs = new TRH_TypeAbsence;
@@ -179,26 +180,26 @@ class InterfaceAbsenceWorkflow
 						$dateInterval = 'le '.dol_print_date($object->date_debut);
 					}
 					else{
-						$dateInterval = 'du '.dol_print_date($object->date_debut).' '.$langs->trans('to').' '.dol_print_date($object->date_fin);	
+						$dateInterval = 'du '.dol_print_date($object->date_debut).' '.$langs->trans('to').' '.dol_print_date($object->date_fin);
 					}
-					
-					
-					
-					$Tab = $PDOdb->ExecuteAsArray("SELECT gu.fk_user 
-						FROM ".MAIN_DB_PREFIX."usergroup_user gu 
-						WHERE gu.fk_usergroup IN ( SELECT DISTINCT fk_usergroup FROM ".MAIN_DB_PREFIX."usergroup_user WHERE fk_user=".$u->id." )	 
-						AND gu.fk_user NOT IN (".$u->id.") 
+
+
+
+					$Tab = $PDOdb->ExecuteAsArray("SELECT gu.fk_user
+						FROM ".MAIN_DB_PREFIX."usergroup_user gu
+						WHERE gu.fk_usergroup IN ( SELECT DISTINCT fk_usergroup FROM ".MAIN_DB_PREFIX."usergroup_user WHERE fk_user=".$u->id." )
+						AND gu.fk_user NOT IN (".$u->id.")
 					");
-					
+
 					foreach($Tab as $row) {
-						
+
 						$uMail = new User($db);
 						$uMail->fetch($row->fk_user);
 
 						if( $uMail->email ) {
-							$TBS=new TTemplateTBS;						
+							$TBS=new TTemplateTBS;
 							$html = $TBS->render( dol_buildpath('/absence/tpl/mail.absence.alert.coworkers.tpl.php')
-								,array() 
+								,array()
 								,array(
 									'mail'=>array(
 										'name'=>$uMail->getNomUrl()
@@ -208,20 +209,20 @@ class InterfaceAbsenceWorkflow
 									)
 								)
 							);
-							
+
 							$rep=new TReponseMail($conf->global->RH_USER_MAIL_SENDER, $uMail->email,"Votre collaborateur sera ".$the_type, $html);
 							$rep->send();
-							
+
 						}
-						
+
 					}
-					
+
 				}
 				*/
 				if($conf->agenda->enabled && !empty($conf->global->RH_ADD_ACTIONCOMM_ON_ABSENCE_VALIDATE)) {
-					
+
 					dol_include_once('/comm/action/class/actioncomm.class.php');
-					
+
 					$a = new ActionComm($db);
 					$a->type_code = 'AC_ABSENCE';
 					$a->userownerid = $object->fk_user;
@@ -235,22 +236,22 @@ class InterfaceAbsenceWorkflow
 					if($res > 0) {
 						$a->add_object_linked('rh_absence', $object->rowid);
 					}
-					
+
 				}
 
 			}
 
-			
+
 			return 0;
-			
+
 		} elseif($action === 'ABSENCE_BEFOREDELETE') {
-			
+
 			if($object->isPresence && $conf->global->RH_RECUP_RULES == 'AUTO') {
-					
-					//TODO check emploi du temps utilisateur pour voir demie journée normalement non travaillée et détermination compteur de récup en +	
+
+					//TODO check emploi du temps utilisateur pour voir demie journée normalement non travaillée et détermination compteur de récup en +
 					$PDOdb=new TPDOdb;
 					$duree = $object->getNbJourPresence($PDOdb);
-			
+
 					if($duree > 0) {
 						$compteur=new TRH_Compteur;
 						if($compteur->load_by_fkuser($PDOdb, $object->fk_user)) {
@@ -258,11 +259,11 @@ class InterfaceAbsenceWorkflow
 							setEventMessage("Compteur de récupération décrémenté de ".$duree." jour(s)");
 						}
 					}
-					
+
 			}
-				
-			
-			
+
+
+
 			if(!empty($conf->global->RH_ADD_ACTIONCOMM_ON_ABSENCE_VALIDATE)) {
 				// On cherche l'événement agenda lié si existant
 				dol_include_once('/comm/action/class/actioncomm.class.php');
@@ -270,21 +271,21 @@ class InterfaceAbsenceWorkflow
 				$event->fetchObjectLinked($object->rowid,'rh_absence',null,'action');
 				$TKeys = array_keys($event->linkedObjectsIds['action']);
 				$event->fetch($event->linkedObjectsIds['action'][$TKeys[0]]);
-				
+
 				if($event->id > 0) {
 					// On supprime le lien + l'événement
 					$event->deleteObjectLinked($object->rowid,'rh_absence',$event->id,'action');
 					$event->delete();
 				}
-				
+
 			}
-			
+
 		}
 
 		return 0;
     }
 
-	
-	
+
+
 
 }
