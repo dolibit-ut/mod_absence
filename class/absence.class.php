9183d33ad1bd332c390dbda83ba5ff48a146df03
quentin <quentin.vial-gouteyron@atm-consulting.fr>
2020-10-06 10:18:51 +0200

    FIX use right conf

--- class/absence.class.php
+++ class/absence.class.php
@@ -3656,11 +3656,11 @@ class TRH_TypeAbsence extends TObjetStd {
 
 	}
 
-	static function getList(&$PDOdb, $isPresence=false, $forceEntityOne = 0) {
+	static function getList(&$PDOdb, $isPresence=false) {
 		global $conf;
 
 		$sql="SELECT rowid FROM ".MAIN_DB_PREFIX."rh_type_absence
-		WHERE entity IN (0,".((! empty($conf->multicompany->enabled) && ! empty($conf->multicompany->transverse_mode)) || !empty($forceEntityOne) ?"1,":"").$conf->entity.")
+		WHERE entity IN (0,".((! empty($conf->multicompany->enabled) && (! empty($conf->multicompany->transverse_mode) || ! empty($conf->global->MULTICOMPANY_TRANSVERSE_MODE))) ? "1," : "").$conf->entity.")
 		AND isPresence=".(int)$isPresence."
 		ORDER BY typeAbsence";
 
